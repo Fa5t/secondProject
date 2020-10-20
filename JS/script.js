a3810aa7ba354914ca282c48ba464a9d2f32c5e8
FaSt <fadee-stas@yandex.ru>
2020-10-20 22:25:22 +0400

    task 2(16)

--- JS/script.js
+++ JS/script.js
@@ -10,13 +10,16 @@ const personalMovieDB = {
     private:false
 };
 
-const a = prompt('Оддие из последних фильмов',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Оддие из последних фильмов',''),
-      d = prompt('На сколько оцените его?','');
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Оддие из последних фильмов',''),
+      b = prompt('На сколько оцените его?','');
+    if( a != null && b != null && a != '' && b != '' && a.length > 50){
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+    } else{
+        console.log('Error');
+    }
+}
 
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
